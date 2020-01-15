68a24df432d2eba9ecfd240494e2535ade7583a5
Владимир <vladimir@MacBook-Air-Vladimir.local>
2020-01-15 08:34:43 +0200

    v833993939

---
+++ src/Controller/Blog.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class Blog extends AbstractController
+{    
+    /**
+     * @Route("/", name="list")
+     */
+    public function listPosts()
+    {
+        return $this->render('posts/list.html.twig');
+    }
+    
+    
+    /**
+     * @Route("/post/{id}/edit", name="edit", methods={"GET"})
+     */
+    public function editPost(int $id)
+    {
+        var_dump($id);
+        return $this->render('posts/edit.html.twig');
+    }
+    
+     /**
+     * @Route("/post/add", name="add", methods={"GET"})
+     */
+    public function addPost()
+    {
+        return $this->render('posts/add.html.twig');
+    }
+    
+      /**
+     * @Route("/post/save", name="save", methods={"POST"})
+     */
+    public function savePost()
+    {
+        var_dump('Post saved'); die;
+    }
+    
+    /**
+     * @Route("/post/{id}", name="show", methods={"GET"})
+     */
+    public function showPost(int $id)
+    {
+        var_dump($id);
+        return $this->render('posts/show.html.twig');
+    }
+    
+  
+    
+}
